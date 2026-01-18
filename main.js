7bb700f3cb3d98486faa3c23d99220ec886142fc
Hammad Khan <hk5718721@gmail.com>
2026-01-18 22:51:20 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,24 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 function App() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1 style={{ fontSize: "32px", fontWeight: "700" }}>
-        EuroTools is working
-      </h1>
-      <p>If you can see this, React loaded correctly.</p>
-    </div>
+  return React.createElement(
+    "div",
+    { style: { padding: "40px" } },
+    React.createElement(
+      "h1",
+      { style: { fontSize: "32px", fontWeight: "700" } },
+      "EuroTools is working"
+    ),
+    React.createElement(
+      "p",
+      null,
+      "If you can see this, React loaded correctly."
+    )
   )
 }
 
 const rootElement = document.getElementById("root")
 
 if (rootElement) {
-  createRoot(rootElement).render(<App />)
+  createRoot(rootElement).render(React.createElement(App))
 }
